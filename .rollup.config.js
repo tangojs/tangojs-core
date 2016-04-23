545f302ada4703d5d2ac64f9f0fc83dfd94d1184
mliszcz <liszcz.michal@gmail.com>
2016-04-24 00:52:51 +0200

    babel and gulp removed; bundling with rollup; output renamed to tangojs-core.js
    closes tangojs/issues#16
    closes tangojs/issues#17

---
+++ .rollup.config.js
@@ -0,0 +1,10 @@
+
+export default {
+  entry: 'src/tangojs-core.js',
+  dest: 'lib/tangojs-core.js',
+  format: 'umd',
+  moduleId: 'tangojs-core',
+  moduleName: 'tangojs',
+  plugins: [],
+  sourceMap: true
+}
