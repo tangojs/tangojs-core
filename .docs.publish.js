839c8e329f7de41682a970dfafa14503d39d40b9
mliszcz <liszcz.michal@gmail.com>
2016-04-28 00:34:28 +0200

    owner and name added to apidocs commit

--- .docs.publish.js
+++ .docs.publish.js
@@ -1,11 +1,18 @@
 
-const version = require('./package.json').version
 const ghPages = require('gh-pages')
-const git = require('git-rev-sync')
+const gitRevSync = require('git-rev-sync')
+const gitUrlParse = require('git-url-parse')
 const path = require('path')
+const pkgjson = require('./package.json')
+
+const gitUrl = gitUrlParse(pkgjson.repository.url)
+const gitRef = `${gitUrl.owner}/${gitUrl.name}@${gitRevSync.short()}`
+const message = `docs for ${pkgjson.version} (${gitRef})`
+
+console.log('publishing:', message)
 
 ghPages.publish(path.join(__dirname, 'docs'), {
   repo: 'git@github.com:tangojs/apidocs.git',
   branch: 'gh-pages',
-  message: `docs for ${version} (@${git.short()})`
+  message: message
 })
